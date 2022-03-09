b701c53f5503effe5786268b0b9b60f95add6378
Dan Possehl <danielbpossehl@gmail.com>
2022-03-09 09:57:18 -0800

    created footer

--- components/footer.js
+++ components/footer.js
@@ -1,2 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: pink;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 40px;
+  padding-right: 40px;
+  @media (max-width: 768px) {
+  }
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <p>616-821-0139</p>
+      <p>BookB33tJuice@gmail.com</p>
+      <p>INSTAGRAM</p>
+      <p>FACEBOOK</p>
+      <p>MEDIA</p>
+    </Container>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from "../styles/Home.module.css";
 import Coverphoto from "../components/coverphoto";
 import About from "../components/about";
 import Videosection from "../components/videosection";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Coverphoto />
       <About />
       <Videosection />
+      <Footer />
     </>
   );
 }
