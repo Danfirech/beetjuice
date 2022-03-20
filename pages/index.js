f23ba8270a419e4913f75f487dec05527f362c35
Dan Possehl <danielbpossehl@gmail.com>
2022-03-20 12:14:18 -0700

    changed error

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Coverphoto from "../components/coverphoto";
 import About from "../components/about";
 import Pricing from "../components/pricing";
 import Footer from "../components/footer";
-import Pic2 from "../components/coverPhoto";
 import Video from "../components/video";
 import MobilePhoto from "../components/mobilePhoto";
 import MobileFooter from "../components/mobileFooter";
@@ -13,7 +12,6 @@ export default function Home() {
   return (
     <>
       <Header />
-      {/* <Coverphoto /> */}
       <Coverphoto />
       <MobilePhoto />
       <About />
