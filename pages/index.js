ffb4c76048bb54c92d9867a488eb3c8d75ce6009
nascott <nascott94@outlook.com>
2022-03-22 10:55:10 -0600

    getting rid of unneeded components

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import styles from '../styles/Home.module.css';
 import Header from '../components/header';
 import Coverphoto from '../components/coverphoto';
 import About from '../components/about';
-import Pricing from '../components/pricing';
 import Footer from '../components/footer';
-import Video from '../components/video';
 // import MobilePhoto from '../components/mobilePhoto';
 import MobileFooter from '../components/mobileFooter';
 
