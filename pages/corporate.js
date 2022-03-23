250c09eefba9c0969ddf15439dd579e6de4c2cc3
nascott <nascott94@outlook.com>
2022-03-23 11:12:24 -0600

    adding corporate page things

---
+++ pages/corporate.js
@@ -0,0 +1,13 @@
+import CorporateHeader from '../components/CorporatePage/CorporateHeader';
+import Pricing from '../components/CorporatePage/Pricing';
+import Footer from '../components/footer';
+
+export default function Corporate() {
+  return (
+    <>
+      <CorporateHeader />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
