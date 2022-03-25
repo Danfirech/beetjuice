c2a8ec68ab0e6b1ebbc4bb0ce7135ed51dc5a1c8
nascott <nascott94@outlook.com>
2022-03-25 10:46:59 -0600

    working on passionfruit page

--- pages/passionfruit.js
+++ pages/passionfruit.js
@@ -1,11 +1,11 @@
-import styles from "../styles/Home.module.css";
-import Header from "../components/header";
-import Coverphoto from "../components/coverphoto";
-import About from "../components/about";
-import Footer from "../components/footer";
+import styles from '../styles/Home.module.css';
+import Header from '../components/header';
+import Coverphoto from '../components/coverphoto';
+import About from '../components/about';
+import Footer from '../components/footer';
 // import MobilePhoto from '../components/mobilePhoto';
-import MobileFooter from "../components/mobileFooter";
-import PassionfruitContainer from "../components/PassionFruitePage/passionfruitcontainer";
+import MobileFooter from '../components/mobileFooter';
+import PassionfruitContainer from '../components/PassionFruitePage/Passionfruitcontainer';
 
 export default function Home() {
   return (
