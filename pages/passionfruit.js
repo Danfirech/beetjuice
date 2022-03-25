5668f065940ba1e40051f066d2ba0d98d761f7d6
nascott <nascott94@outlook.com>
2022-03-25 17:39:58 -0600

    adding poster to passionfruit page

--- pages/passionfruit.js
+++ pages/passionfruit.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css';
 import Header from '../components/header';
 import Coverphoto from '../components/coverphoto';
 import About from '../components/about';
-import Footer from '../components/footer';
+import Footer from '../components/WeddingCorporatePage/weddingfooter';
 // import MobilePhoto from '../components/mobilePhoto';
 import MobileFooter from '../components/mobileFooter';
 import PassionfruitContainer from '../components/PassionFruitePage/Passionfruitcontainer';
@@ -12,8 +12,8 @@ export default function Home() {
     <>
       <Header />
       <PassionfruitContainer />
-      <Footer />
       <MobileFooter />
+      {/* <Footer /> */}
     </>
   );
 }
