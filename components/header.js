36f83cb3c5cb142aa0b9c37d1b377dd274727b7c
Dan Possehl <danielbpossehl@gmail.com>
2022-03-29 10:33:16 -0700

    test

--- components/header.js
+++ components/header.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Image from 'next/image';
-import logo from '../media/Transparentlogo.jpeg';
-import Link from 'next/link';
-import { MdClose, MdMenu } from 'react-icons/md';
-import '../pages/_document';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import logo from "../media/Transparentlogo.jpeg";
+import Link from "next/link";
+import { MdClose, MdMenu } from "react-icons/md";
+import "../pages/_document";
 
 const Container = styled.div`
   height: 100px;
@@ -46,7 +46,7 @@ const Right = styled.div`
   align-items: center;
   justify-content: space-between;
   padding-right: 58px;
-  font-family: 'Overpass Mono', monospace;
+  font-family: "Overpass Mono", monospace;
   border-bottom: 1px solid black;
   padding-left: 250px;
   @media (max-width: 768px) {
