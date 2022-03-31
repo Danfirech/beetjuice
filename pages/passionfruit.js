96dce72a3f630d603bd4ad0b5037b1d1484b6903
Dan Possehl <danielbpossehl@gmail.com>
2022-03-31 16:53:25 -0700

    fixed

--- pages/passionfruit.js
+++ pages/passionfruit.js
@@ -1,11 +1,11 @@
-import styles from '../styles/Home.module.css';
-import Header from '../components/header';
-import Coverphoto from '../components/coverphoto';
-import About from '../components/about';
-import Footer from '../components/WeddingCorporatePage/weddingfooter';
+import styles from "../styles/Home.module.css";
+import Header from "../components/header";
+import Coverphoto from "../components/coverphoto";
+import About from "../components/about";
+import Footer from "../components/WeddingCorporatePage/weddingfooter";
 // import MobilePhoto from '../components/mobilePhoto';
-import MobileFooter from '../components/mobileFooter';
-import PassionfruitContainer from '../components/PassionFruitePage/Passionfruitcontainer';
+import MobileFooter from "../components/mobileFooter";
+import PassionfruitContainer from "../components/PassionFruitePage/passionfruitcontainer";
 
 export default function Home() {
   return (
@@ -13,7 +13,6 @@ export default function Home() {
       <Header />
       <PassionfruitContainer />
       <MobileFooter />
-      {/* <Footer /> */}
     </>
   );
 }
