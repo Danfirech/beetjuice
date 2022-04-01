e578c174b6431759e640bcb82eda9687c1473419
Dan Possehl <danielbpossehl@gmail.com>
2022-03-31 21:47:40 -0700

    spaced out about

--- components/about.js
+++ components/about.js
@@ -22,7 +22,7 @@ const Container = styled.div`
     padding-left: 50px;
     padding-right: 50px;
     padding-bottom: 120px;
-    padding-top: 50px;
+    padding-top: 220px;
     text-align: left;
   }
 `;
