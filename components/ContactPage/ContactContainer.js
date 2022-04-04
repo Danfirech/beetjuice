b210069b8049e3d117d305e7eaabf4d541e4de3d
Dan Possehl <danielbpossehl@gmail.com>
2022-04-04 11:11:05 -0700

    contact form imbeded

--- components/ContactPage/ContactContainer.js
+++ components/ContactPage/ContactContainer.js
@@ -1,26 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import JotformEmbed from "react-jotform-embed";
 
-const Container = styled.div`
-  height: 100vh;
+const ContactContainer = styled.div`
+  height: 1300px;
   width: 100vw;
   background-color: #e5e4e2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 1068px) {
+  }
+  @media (max-width: 868px) {
+    flex-direction: column;
+    height: 920px;
+    padding-bottom: 330px;
+  }
 `;
 
-const ContactForm = styled.div`
-  height: 200px;
-  width: 100px;
-  background-color: pink;
+const FormContainer = styled.div`
+  height: 500px;
+  width: 500px;
+  background-color: #e5e4e2;
+  margin-top: -900px;
+  margin-bottom: -400px;
+  @media (max-width: 1068px) {
+    width: 100%;
+    height: 50px;
+  }
+  @media (max-width: 868px) {
+    width: 100%;
+    height: 50px;
+  }
 `;
 
-const Contact = () => {
+const ContactForm = () => {
   return (
     <>
-      <Container>
-        <ContactForm></ContactForm>
-      </Container>
+      <ContactContainer>
+        <FormContainer>
+          <JotformEmbed src="https://form.jotform.com/220784862221152" />
+        </FormContainer>
+      </ContactContainer>
     </>
   );
 };
 
-export default Contact;
+export default ContactForm;
